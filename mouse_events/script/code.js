d3ba746a543bade42af6b06aacc63a09a945ca8b
Nusaibah1 <charifnusaibah01@gmail.com>
2025-08-17 07:38:28 +0200

    day-one

---
+++ mouse_events/script/code.js
@@ -0,0 +1,17 @@
+let mouseX = 0;
+let mouseY = 0;
+
+
+const coordinates = document.querySelector("#coordinates strong");
+
+
+window.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    if (coordinates) {
+        coordinates.innerT
+    }
+});
+
+
+
